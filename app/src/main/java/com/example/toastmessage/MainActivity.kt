d44845d9db12a7964d0bbcc2b0268a4e536436e6
Anshi Kandulna <24je0591@iitism.ac.in>
2024-12-13 13:58:01 +0530

    added explicit and implicit intents

--- app/src/main/java/com/example/toastmessage/MainActivity.kt
+++ app/src/main/java/com/example/toastmessage/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.toastmessage
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     private var isDarkMode = false
     private var isReadMode = false
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "UnsafeIntentLaunch")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -68,5 +72,25 @@ class MainActivity : AppCompatActivity() {
             isReadMode=!isReadMode
             isDarkMode=false  //reset dark mode
         }
+
+        val buttonNext=findViewById<Button>(R.id.btnNext) //button for going to MainActivity2
+        buttonNext.setOnClickListener {
+            val intent= Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        val buttonCamera=findViewById<CardView>(R.id.cardCamera) //button for opening camera
+        buttonCamera.setOnClickListener {
+            val intentCamera=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intentCamera)
+        }
+
+        val buttonGoogle=findViewById<CardView>(R.id.cardGoogle) //button for opening google
+        buttonGoogle.setOnClickListener {
+            val intentGoogle=Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com"))
+            startActivity(intentGoogle)
+        }
+
+
     }
 }
